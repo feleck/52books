fe681f4b4a0ef102932a27c10054daba8dd1fe38
Marcin Felczak <feleck@poczta.onet.pl>
2017-05-24 15:11:55 +0200

    add basic config for localization (polish)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module BookPerWeek
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.available_locales = [:en, :pl]
   end
 end
